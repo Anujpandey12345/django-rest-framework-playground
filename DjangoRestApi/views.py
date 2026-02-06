745e275fa4004afcd6f7fb8cbe7c6870adf20331
Anuj Kumar <anujpandey61687@gmail.com>
2026-02-06 23:41:38 +0530

    Refactor Django project structure and organize apps

--- DjangoRestApi/views.py
+++ DjangoRestApi/views.py
@@ -3,30 +3,45 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
+import io
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 
 
 def Student_detail(request, pk):
-    stu = Student.objects.get(id = pk)
-    # print(f"This is STU{stu}")
+    stu = Student.objects.get(id=pk)
     serializer = StudentSerializer(stu)
-    # print(f"This is Serializer{serializer}")
     json_data = JSONRenderer().render(serializer.data)
-    # print(f"This is json_data{json_data}")
-    return HttpResponse(json_data, content_type = 'application/json')
-
-
+    return HttpResponse(json_data, content_type='application/json')
 
 
 # Query Set -> All students Data
 
 
-def Student_list(reques):
+def Student_list(request):
     stu = Student.objects.all()
-    # print(f"This is STU{stu}")
-    serializer = StudentSerializer(stu, many = True)
-    # print(f"This is Serializer{serializer}")
+    serializer = StudentSerializer(stu, many=True)
     json_data = JSONRenderer().render(serializer.data)
-    # print(f"This is json_data{json_data}")
-    return HttpResponse(json_data, content_type = 'application/json')
+    return HttpResponse(json_data, content_type='application/json')
+
+
+@csrf_exempt
+def student_create(request):
+    if request.method == "POST":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=pythondata)
+
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Inserted'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type="application/json")
+        else:
+            json_data = JSONRenderer().render(serializer.errors)
+            return HttpResponse(json_data, content_type="application/json")
+    return HttpResponse('Method not allowed', status=405)
